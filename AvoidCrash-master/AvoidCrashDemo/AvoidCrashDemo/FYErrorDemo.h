cb01dca101a2f1bc6cdf3c4c0b3ba03611dd77d3
陈波涛 <503623373@qq.com>
2019-05-23 18:37:51 +0800

    相关提交

---
+++ AvoidCrash-master/AvoidCrashDemo/AvoidCrashDemo/FYErrorDemo.h
@@ -0,0 +1,19 @@
+//
+//  FYErrorDemo.h
+//  AvoidCrashDemo
+//
+//  Created by 陈波涛 on 2019/5/20.
+//  Copyright © 2019 chenfanfang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FYErrorDemo : NSObject
+
+- (void)doErrorAction;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ AvoidCrash-master/AvoidCrashDemo/AvoidCrashDemo/FYErrorDemo.m
@@ -0,0 +1,13 @@
+//
+//  FYErrorDemo.m
+//  AvoidCrashDemo
+//
+//  Created by 陈波涛 on 2019/5/20.
+//  Copyright © 2019 chenfanfang. All rights reserved.
+//
+
+#import "FYErrorDemo.h"
+
+@implementation FYErrorDemo
+
+@end
